d6519f5f0ecab331bef0453a5a4d65e431026e1f
root <root@ramses.Ramses-Debian>
2013-10-10 00:19:41 -0300

    Refatorando os casos de teste do HockeyPlayerTest.java

--- test/model/HockeyPlayerTest.java
+++ test/model/HockeyPlayerTest.java
@@ -1,12 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -116,198 +109,51 @@ public class HockeyPlayerTest {
         assertEquals("bom", novoHockeyPlayer.getPuckControl());
     }
 
-    /**
-     * Test of setPuckControl method, of class HockeyPlayer.
-     */
-    @Test
-    public void testSetPuckControl() {
-        System.out.println("setPuckControl");
-        String puckControl = "null";
-        novoHockeyPlayer.setPuckControl(puckControl);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of getShotBlocking method, of class HockeyPlayer.
-     */
     @Test
     public void testGetShotBlocking() {
-        System.out.println("getShotBlocking");
-        String expResult = null;
-        String result = novoHockeyPlayer.getShotBlocking();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+        novoHockeyPlayer.setShotBlocking("excelente");
+        assertEquals("excelente", novoHockeyPlayer.getShotBlocking());
     }
 
-    /**
-     * Test of setShotBlocking method, of class HockeyPlayer.
-     */
-    @Test
-    public void testSetShotBlocking() {
-        System.out.println("setShotBlocking");
-        String shotBlocking = "null";
-        novoHockeyPlayer.setShotBlocking(shotBlocking);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of getSlapShotAccuracy method, of class HockeyPlayer.
-     */
     @Test
     public void testGetSlapShotAccuracy() {
-        System.out.println("getSlapShotAccuracy");
-        String expResult = null;
-        String result = novoHockeyPlayer.getSlapShotAccuracy();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+        novoHockeyPlayer.setSlapShotAccuracy("bom");
+        assertEquals("bom", novoHockeyPlayer.getSlapShotAccuracy());
     }
 
-    /**
-     * Test of setSlapShotAccuracy method, of class HockeyPlayer.
-     */
-    @Test
-    public void testSetSlapShotAccuracy() {
-        System.out.println("setSlapShotAccuracy");
-        String slapShotAccuracy = "null";
-        novoHockeyPlayer.setSlapShotAccuracy(slapShotAccuracy);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of getSlapShotPower method, of class HockeyPlayer.
-     */
     @Test
     public void testGetSlapShotPower() {
-        System.out.println("getSlapShotPower");
-        String expResult = null;
-        String result = novoHockeyPlayer.getSlapShotPower();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+        novoHockeyPlayer.setSlapShotPower("bom");
+        assertEquals("bom", novoHockeyPlayer.getSlapShotPower());
     }
 
-    /**
-     * Test of setSlapShotPower method, of class HockeyPlayer.
-     */
-    @Test
-    public void testSetSlapShotPower() {
-        System.out.println("setSlapShotPower");
-        String slapShotPower = "null";
-        novoHockeyPlayer.setSlapShotPower(slapShotPower);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of getWristShotAccuracy method, of class HockeyPlayer.
-     */
     @Test
     public void testGetWristShotAccuracy() {
-        System.out.println("getWristShotAccuracy");
-        String expResult = null;
-        String result = novoHockeyPlayer.getWristShotAccuracy();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+        novoHockeyPlayer.setWristShotAccuracy("bom");
+        assertEquals("bom", novoHockeyPlayer.getWristShotAccuracy());
     }
 
-    /**
-     * Test of setWristShotAccuracy method, of class HockeyPlayer.
-     */
-    @Test
-    public void testSetWristShotAccuracy() {
-        System.out.println("setWristShotAccuracy");
-        String wristShotAccuracy = "null";
-        novoHockeyPlayer.setWristShotAccuracy(wristShotAccuracy);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of getWristShotPower method, of class HockeyPlayer.
-     */
     @Test
     public void testGetWristShotPower() {
-        System.out.println("getWristShotPower");
-        String expResult = null;
-        String result = novoHockeyPlayer.getWristShotPower();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+        novoHockeyPlayer.setWristShotPower("bom");
+        assertEquals("bom", novoHockeyPlayer.getWristShotPower());
     }
 
-    /**
-     * Test of setWristShotPower method, of class HockeyPlayer.
-     */
-    @Test
-    public void testSetWristShotPower() {
-        System.out.println("setWristShotPower");
-        String wristShotPower = "null";
-        novoHockeyPlayer.setWristShotPower(wristShotPower);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of getSpeed method, of class HockeyPlayer.
-     */
     @Test
     public void testGetSpeed() {
-        System.out.println("getSpeed");
-        Double expResult = 0.00;
-        Double result = novoHockeyPlayer.getSpeed();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+        novoHockeyPlayer.setSpeed(20.5);
+        assertEquals(20.5, novoHockeyPlayer.getSpeed(), 0.01);
     }
 
-    /**
-     * Test of setSpeed method, of class HockeyPlayer.
-     */
-    @Test
-    public void testSetSpeed() {
-        System.out.println("setSpeed");
-        Double speed = 0.00;
-        novoHockeyPlayer.setSpeed(speed);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of getStickChecking method, of class HockeyPlayer.
-     */
     @Test
     public void testGetStickChecking() {
-        System.out.println("getStickChecking");
-        String expResult = null;
-        String result = novoHockeyPlayer.getStickChecking();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+        novoHockeyPlayer.setStickChecking("bom");
+        assertEquals("bom", novoHockeyPlayer.getStickChecking());
     }
 
-    /**
-     * Test of setStickChecking method, of class HockeyPlayer.
-     */
-    @Test
-    public void testSetStickChecking() {
-        System.out.println("setStickChecking");
-        String stickChecking = "null";
-        novoHockeyPlayer.setStickChecking(stickChecking);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of getStrength method, of class HockeyPlayer.
-     */
     @Test
     public void testGetStrength() {
-        System.out.println("getStrength");
-        String expResult = null;
-        String result = novoHockeyPlayer.getStrength();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of setStrength method, of class HockeyPlayer.
-     */
-    @Test
-    public void testSetStrength() {
-        System.out.println("setStrength");
-        String strength = "null";
-        novoHockeyPlayer.setStrength(strength);
-        // TODO review the generated test code and remove the default call to fail.
+        novoHockeyPlayer.setStrength("boa");
+        assertEquals("boa", novoHockeyPlayer.getStrength());
     }
 }
